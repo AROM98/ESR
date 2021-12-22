260aae18d0c10af98a8e9f0e7ec4607b323df8c5
AROM98 <rafa.olim@sapo.pt>
2021-12-22 18:58:11 +0000

    nova classe ServerAnswerThread, para responder a pedidos do servidor

---
+++ src/ServerAnswerThread.java
@@ -0,0 +1,122 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ServerAnswerThread implements Runnable{
+
+
+    private Bootstrapper bootstrapper;
+    private byte[] dados;
+    private int porta;
+    private ReentrantLock lock;
+    private HashMap<Integer, Integer> portas;
+
+    /**
+     * Recebe argumentos inicias para conseguir responder
+     */
+    public ServerAnswerThread(Bootstrapper b, byte[] dados, HashMap<Integer, Integer> portas){
+        this.porta = 81;
+        this.bootstrapper = b;
+        this.dados = dados;
+        this.portas = portas;
+    }
+
+    public void run() {
+
+        Integer flag = -1;
+        String ipNode;
+        String[] res;
+        String data = new String(dados);
+        res = data.split(" ");
+        flag = Integer.parseInt(res[0]);
+        //System.out.println("flag -> " + flag); //devo obter a flag aqui
+
+        switch (flag){
+            case 0 :
+                // 0-> beacons (nodo -> servidor)
+                // criar tabela com timestamps ? mas isto teria que ser mantido por uma thread, não?
+                //atualizar coisas no bootstrap, se não estou enganado!
+                ipNode = res[1];
+
+
+
+                break;
+            case 1:
+
+                //1-> pede stream (nodo(cliente) -> servidor)
+                //tenho que iniciar a stream para esse cliente, para isso tenho que usar o bootstrapper para
+                //saber qual o caminho
+                //ao criar o stream para uma porta privada, não esquecer de verificar o seu status!!
+                // neste caso enviar pacote com a porta da stream
+
+                String ficheiro = res[1];
+                ipNode = res[2];
+
+                String videoFileName = ficheiro; // se a ordem enviada for flag ++ fich
+
+                Socket clientSocket = null;
+                OutputStream out = null;
+                try {
+                    clientSocket = new Socket(ipNode, porta);
+                    out = clientSocket.getOutputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //buscar porta disponivel e meter na portaStream
+                int port = ServerF.GetAvailablePort(portas);
+                String portaStream = Integer.toString(port); //porta do upd;
+                byte[] tmp = portaStream.getBytes();
+                try {
+                    ByteMessages.sendBytes(tmp, out);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //mandar a mensagem aos nodos intermediarios
+                ArrayList<String> trajeto = new ArrayList<>(); //trajeto do servidor ao cliente
+                for (String ipNodeInter : trajeto) {
+
+                    try {
+                        clientSocket = new Socket(ipNodeInter, porta);
+                        out = clientSocket.getOutputStream();
+
+                        String tmpV = portaStream + " " + ipNodeInter;
+                        tmp = tmpV.getBytes();
+                        ByteMessages.sendBytes(tmp, out);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                StreamSender s = new StreamSender(videoFileName,ipNode,Integer.parseInt(portaStream));
+                break;
+
+            case 4:
+                //acabar ligação (...) (ambos sentidos)
+
+                break;
+            case 5:
+                lock.lock();
+                int port_to_close = Integer.parseInt(res[1]);
+                portas.put(port_to_close, 0); // porta passa a estar desactivada
+                lock.unlock();
+                break;
+
+            case 6:
+                //6-> nodo apaga ficheiro da cache (nodo -> servidor)
+                // vou atualizar informação no bootstrapper --
+                // "o que vou querer é o IP de quem apagou da cache e o id do file"
+
+                break;
+
+            default:
+                // se chegou aqui é porque se calhar deu erro no parse.
+                // ou não veio alguma destas flags
+                System.out.println("alguma coisa correu mal.");
+        }
+    }
+}
